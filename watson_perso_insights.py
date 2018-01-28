07dda57603e85f614f05ef3e87d5f3b3e30520a8
tristantoupin <tristantoupin@gmail.com>
2018-01-28 10:21:48 -0500

    watson working aka final

--- watson_perso_insights.py
+++ watson_perso_insights.py
@@ -3,15 +3,13 @@ from os.path import join, dirname
 from watson_developer_cloud import PersonalityInsightsV2
 import sys
 
+text = sys.argv[1]
 
 personality_insights = PersonalityInsightsV2(
     username='245c8319-f709-4a86-960c-0864b633ddac',
     password='7MBXhiFtmKi1')
 
-
-with open(join(dirname(__file__), 'personality.txt')) as personality_text:
-    json_result = (json.dumps(personality_insights.profile(text=personality_text.read()), indent=2))
-
+json_result = (json.dumps(personality_insights.profile(text=text), indent=2))
 
 
 with open("output.txt", "w") as text_file:
