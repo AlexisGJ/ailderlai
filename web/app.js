e658487c99708a05ca22f0f51b50883ff8bf5c43
egeozer <ege.ozer@mail.mcgill.ca>
2018-01-28 10:40:05 -0500

    fixed timer

--- web/app.js
+++ web/app.js
@@ -182,37 +182,47 @@ function sendSMSText() {
 
     // TIMER
         
-    var seconds = 0;
-    var minutes = 0;
-    var hours = 0;
-    var t;
-    var isActive = false;
+    var h1 = document.getElementsByTagName('h2')[0],
+    start = document.getElementById('asr_go'),
+    seconds = 0, minutes = 0, hours = 0,
+    t;
+    var isActivee = false;
     function add() {
-
-        seconds++;
-        if (seconds >= 60) {
-            seconds = 0;
-            minutes++;
-            if (minutes >= 60) {
-                minutes = 0;
-                hours++;
-            }
+      
+    seconds++;
+    if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes >= 60) {
+            minutes = 0;
+            hours++;
         }
-
-        var timenow = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
-        $timerText.html();
-        timer();
+    }
+    
+    h1.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+    timer();
     }
     function timer() {
-
-        t = setTimeout(add, 1000);
-
+      
+    t = setTimeout(add, 1000);
+     
+    }
+    start.onclick = function(){
+      if(!isActive){
+      timer();
+      isActivee = true;
+    }
+    else{
+      h1.textContent = "00:00:00";
+      seconds = 0; minutes = 0; hours = 0;
+      clearTimeout(t);
+      isActivee = false;
+    
+    }
     }
     timer();
     clearTimeout(t);
-
-    // END TIMER
-
+    
 
 
 
