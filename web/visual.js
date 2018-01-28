a9b3adb3133d2bbad8636da58d153dfc69b682ad
egeozer <ege.ozer@mail.mcgill.ca>
2018-01-28 08:09:14 -0500

    Added Timer in visual.html

---
+++ web/visual.js
@@ -0,0 +1,48 @@
+
+var h1 = document.getElementsByTagName('h2')[0],
+start = document.getElementById('asr_go'),
+seconds = 0, minutes = 0, hours = 0,
+t;
+var isActive = false;
+function add() {
+  
+seconds++;
+if (seconds >= 60) {
+    seconds = 0;
+    minutes++;
+    if (minutes >= 60) {
+        minutes = 0;
+        hours++;
+    }
+}
+
+h1.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+timer();
+}
+function timer() {
+  
+t = setTimeout(add, 1000);
+ 
+}
+start.onclick = function(){
+  if(!isActive){
+  timer();
+  isActive = true;
+}
+else{
+  h1.textContent = "00:00:00";
+  seconds = 0; minutes = 0; hours = 0;
+  clearTimeout(t);
+  isActive = false;
+
+}
+}
+timer();
+clearTimeout(t);
+
+
+/* Clear button */
+clear.onclick = function() {
+h1.textContent = "00:00:00";
+seconds = 0; minutes = 0; hours = 0;
+}
