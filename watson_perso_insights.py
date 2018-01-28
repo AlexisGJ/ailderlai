d9a0a0f77d0d9f692b537a924f81f0e151d44dd8
tristantoupin <tristantoupin@gmail.com>
2018-01-28 10:38:21 -0500

    fix

--- watson_perso_insights.py
+++ watson_perso_insights.py
@@ -9,8 +9,13 @@ personality_insights = PersonalityInsightsV2(
     username='245c8319-f709-4a86-960c-0864b633ddac',
     password='7MBXhiFtmKi1')
 
+print (text)
+
+
 json_result = (json.dumps(personality_insights.profile(text=text), indent=2))
 
+
+print (json_result)
 
 with open("output.txt", "w") as text_file:
 	text_file.write(json_result.replace("\n",""))
