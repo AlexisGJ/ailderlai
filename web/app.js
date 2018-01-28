538dba41c60fe4ea6879a83a992ece2c5fc82c85
kovalexis <alexis.giguere-joannette@mail.mcgill.ca>
2018-01-28 10:16:26 -0500

    Added timer

--- web/app.js
+++ web/app.js
@@ -185,7 +185,11 @@ function sendSMSText() {
         if(isRecording) {
             Nuance.stopASR();
             $asrLabel.text('RECORD');
-            $timerText.html('Timer stopped');
+            
+            $timerText.html("00:00:00");
+            seconds = 0; minutes = 0; hours = 0;
+            clearTimeout(t);
+            isActive = false;
         } else {
             cleanViz();
             var options = createOptions({
@@ -199,11 +203,49 @@ function sendSMSText() {
             }
             Nuance.startASR(options);
             $asrLabel.text('STOP RECORDING');
-            $timerText.html('Timer started');
+            timer();
         }
         isRecording = !isRecording;
     }
     $asrRecord.on('click', asr);
+
+    // TIMER
+    
+    var h1 = document.getElementsByTagName('h2')[0],
+    start = document.getElementById('asr_go'),
+    seconds = 0, minutes = 0, hours = 0,
+    t;
+    var isActive = false;
+    function add() {
+    
+        seconds++;
+        if (seconds >= 60) {
+            seconds = 0;
+            minutes++;
+            if (minutes >= 60) {
+                minutes = 0;
+                hours++;
+            }
+        }
+
+        $timerText.html(hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+        timer();
+    }
+    function timer() {
+    
+        t = setTimeout(add, 1000);
+    
+    }
+    timer();
+    clearTimeout(t);
+    
+    // END TIMER
+
+
+
+
+    
+        
 
     // TTS
 
