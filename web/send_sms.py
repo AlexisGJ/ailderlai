455d69f67b23bf0e0e19f4c2b1d599e4f167c1f0
kovalexis <alexis.giguere-joannette@mail.mcgill.ca>
2018-01-28 03:01:00 -0500

    Update phone number

--- web/send_sms.py
+++ web/send_sms.py
@@ -9,6 +9,6 @@ auth_token = "fab5ac701ecb3b4b22cd78d1eac5ed7c"
 client = Client(account_sid, auth_token)
 
 client.api.account.messages.create(
-   to="+15148653363",
+   to="+14388635345",
    from_="+15146125782",
    body="Ege lache le cell a tris")
