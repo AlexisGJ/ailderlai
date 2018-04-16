2d520f3e941bcaf22532240b4d09fdb9c0897d4c
AlexisGJ <alexis.giguere-joannette@mail.mcgill.ca>
2018-04-16 14:47:35 -0400

    Small fix

--- web/send_sms.py
+++ web/send_sms.py
@@ -9,6 +9,6 @@ auth_token = "fab5ac701ecb3b4b22cd78d1eac5ed7c"
 client = Client(account_sid, auth_token)
 
 client.api.account.messages.create(
-   to="+14388635345",
-   from_="+15146125782",
+   to="",
+   from_="",
    body="Hi, I have an emergency right now.")
